2716411686b5dc32c2b7f8d78fa59fd070989f31
cannduran <canahmetduran0@gmail.com>
2024-06-19 22:08:16 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Can Ahmet Duran on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
